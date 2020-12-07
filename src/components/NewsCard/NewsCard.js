65010432576ed42aa1298632ac148de7547d3ab1
Edi <selimi.edi.1991@gmail.com>
2020-12-07 12:29:35 +0100

    News Card

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,11 +1,32 @@
 import React from 'react'
-import NewsCards from '../NewsCards/NewsCards'
+import NewsCards from '../NewsCards/NewsCards';
+import { Card, CardActions, CardActionArea, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
 
-const NewsCard = () => {
+const NewsCard = ({ article: { description, publishedAt, source, title, url, urlToImage }, i }) => {
     return (
-        <div>
-            News Card
-        </div>
+        <Card>
+            <CardActionArea>
+                <CardMedia image={urlToImage  || 'https://www.industry.gov.au/sites/default/files/August%202018/image/news-placeholder-738.png'}/>
+                <div>
+                    <Typography variant='body2'
+                        color="textSecondary"
+                        component="h2">
+                    </Typography>
+                    <Typography variant='body2'
+                        color="textSecondary"
+                        component="h2">
+                    </Typography>
+                </div>
+                <Typography gutterBottom var="h5"></Typography>
+                <CardContent>
+                    <Typography variant="body2" color="textSecondary" component="p"></Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <Button size="small" color="primary"></Button>
+                <Typography variant="h5" color="textSecondary"></Typography>
+            </CardActions>
+        </Card>
     )
 }
 
